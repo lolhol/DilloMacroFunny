a07f34dc9cb365f2e567157a153cb9d72f753632
godbrigero <golden.notch.creeper@gmail.com>
2023-07-04 12:10:05 -0700

    Add additional checks to "is in range" module

--- src/main/java/com/dillo/MITGUI/GUIUtils/DilloRouteUtils/IsInBlockRange.java
+++ src/main/java/com/dillo/MITGUI/GUIUtils/DilloRouteUtils/IsInBlockRange.java
@@ -4,12 +4,15 @@ import com.dillo.dilloUtils.BlockUtils.fileUtils.localizedData.currentRoute;
 import com.dillo.utils.DistanceFromTo;
 import com.dillo.utils.previous.random.ids;
 
+import static com.dillo.MITGUI.GUIUtils.MatchServer.IsChecked.isChecked;
+
 public class IsInBlockRange {
 
   public static boolean isInCheckRange() {
     if (
       currentRoute.currentRoute.size() < 1 ||
-      DistanceFromTo.distanceFromTo(ids.mc.thePlayer.playerLocation, currentRoute.currentRoute.get(0)) > 120
+      DistanceFromTo.distanceFromTo(ids.mc.thePlayer.playerLocation, currentRoute.currentRoute.get(0)) > 120 ||
+      isChecked()
     ) {
       return false;
     }
