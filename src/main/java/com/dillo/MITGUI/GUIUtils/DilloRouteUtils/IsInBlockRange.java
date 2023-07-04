b76d4ac4ffefb54395c3dfceb4d22829309a3961
godbrigero <golden.notch.creeper@gmail.com>
2023-07-04 12:07:09 -0700

    Rework is in range checks

--- src/main/java/com/dillo/MITGUI/GUIUtils/DilloRouteUtils/IsInBlockRange.java
+++ src/main/java/com/dillo/MITGUI/GUIUtils/DilloRouteUtils/IsInBlockRange.java
@@ -2,24 +2,15 @@ package com.dillo.MITGUI.GUIUtils.DilloRouteUtils;
 
 import com.dillo.dilloUtils.BlockUtils.fileUtils.localizedData.currentRoute;
 import com.dillo.utils.DistanceFromTo;
-import com.dillo.utils.previous.SendChat;
 import com.dillo.utils.previous.random.ids;
-import net.minecraft.util.BlockPos;
 
 public class IsInBlockRange {
 
   public static boolean isInCheckRange() {
-    double curFurthest = 0;
-
-    for (BlockPos block : currentRoute.currentRoute) {
-      double dist = DistanceFromTo.distanceFromTo(block, ids.mc.thePlayer.getPosition());
-
-      if (dist > curFurthest) {
-        curFurthest = dist;
-      }
-    }
-
-    if (curFurthest > 120 || currentRoute.currentRoute.size() < 1) {
+    if (
+      currentRoute.currentRoute.size() < 1 ||
+      DistanceFromTo.distanceFromTo(ids.mc.thePlayer.playerLocation, currentRoute.currentRoute.get(0)) > 120
+    ) {
       return false;
     }
 
