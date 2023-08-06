62551e18a61bd318277e666c8e977f2b44a8412d
brigerodev <119545560+manshave@users.noreply.github.com>
2023-08-05 22:49:19 -0500

    Tweeked the auto save config to FINALLY work and not bug out

--- src/main/java/com/dillo/commands/AddBlockRouteCommand.java
+++ src/main/java/com/dillo/commands/AddBlockRouteCommand.java
@@ -1,14 +1,9 @@
 package com.dillo.commands;
 
-import com.dillo.main.files.localizedData.currentRoute;
-import com.dillo.main.files.readwrite.AddBlockToRoute;
-import com.dillo.utils.previous.chatUtils.SendChat;
-import com.dillo.utils.previous.random.ids;
-import com.dillo.utils.previous.random.prefix;
+import static com.dillo.main.route.Utils.AddBlockRoute.addBlockRoute;
+
 import gg.essential.api.commands.Command;
 import gg.essential.api.commands.DefaultHandler;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.BlockPos;
 
 public class AddBlockRouteCommand extends Command {
 
@@ -18,24 +13,6 @@ public class AddBlockRouteCommand extends Command {
 
   @DefaultHandler
   public void handle() {
-    BlockPos pos = new BlockPos(ids.mc.thePlayer.posX, ids.mc.thePlayer.posY - 1, ids.mc.thePlayer.posZ);
-    //RenderPoints.renderPoint(null, 0, false);
-
-    if (ids.mc.theWorld.getBlockState(pos).getBlock() == Blocks.cobblestone) {
-      if (currentRoute.currentRouteFile != null) {
-        BlockPos block = new BlockPos(ids.mc.thePlayer.posX, ids.mc.thePlayer.posY - 1, ids.mc.thePlayer.posZ);
-
-        if (!currentRoute.currentRoute.contains(block)) {
-          AddBlockToRoute.addBlockToRoute(block, currentRoute.currentRouteFile);
-          SendChat.chat(prefix.prefix + "Block has been added!");
-        } else {
-          SendChat.chat(prefix.prefix + "You have already added this block to your route! Please do not add it more.");
-        }
-      } else {
-        SendChat.chat(prefix.prefix + "You have not selected a route! Please select one or run /helpMIT!");
-      }
-    } else {
-      SendChat.chat(prefix.prefix + "Cannot add block, The block MUST be a cobblestone!");
-    }
+    addBlockRoute();
   }
 }
